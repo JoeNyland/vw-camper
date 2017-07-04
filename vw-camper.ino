eb57a84115b074d1395843fea7774d18773b4484
Joe Nyland <joenyland@me.com>
2017-07-04 20:15:44 +0100

    /max/min/

--- vw-camper.ino
+++ vw-camper.ino
@@ -4,7 +4,7 @@
 int sensor = A0;         // Sensor is connected on analogue pin 0
 int sensorValue;         // Where sensor value is stored
 int lights = 3;          // Pin that the lighting system is connected to
-int maxDayLevel = 900;   // Max light level for day
+int minDayLevel = 900;   // Min light level for day
 int minNightLevel = 200; // Min light level for night
 
 void setup() {
@@ -13,7 +13,7 @@ void setup() {
 void loop() {
   if (hour() >= 18 && hour() < 22) { // It's currently within hours of operation (18:00 - 22:00)
     sensorValue = analogRead(sensor);
-    if (sensorValue > minNightLevel && sensorValue < maxDayLevel) { // Light level is within the disired range
+    if (sensorValue > minNightLevel && sensorValue < minDayLevel) { // Light level is within the disired range
       lightsOn(sensorValue / 8); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
       delay(10);
       return;
