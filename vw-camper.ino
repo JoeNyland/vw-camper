901d340abdd0cf24d002ae1649abcabb0012516c
Joe Nyland <joenyland@me.com>
2017-09-20 13:27:55 +0100

    Remove minNightLevel

--- vw-camper.ino
+++ vw-camper.ino
@@ -7,7 +7,6 @@ int sensor = A0;         // Sensor is connected on analogue pin 0
 int sensorValue;         // Where sensor value is stored
 int lights = 3;          // Pin that the lighting system is connected to
 int minDayLevel = 900;   // Min light level for day
-int minNightLevel = 50;  // Min light level for night
 int pollDelay = 300000;  // ms between checking the light level
 
 void setup() {
@@ -18,7 +17,7 @@ void loop() {
   DateTime now = rtc.now();
   if (now.hour() >= 18 && now.hour() < 22) { // It's currently within hours of operation (18:00 - 22:00)
     sensorValue = analogRead(sensor);
-    if (sensorValue > minNightLevel && sensorValue < minDayLevel) { // Light level is within the desired range
+    if (sensorValue < minDayLevel) { // Light level is within the desired range
       lightsOn(sensorValue / 8); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
       delay(pollDelay);
       return;
