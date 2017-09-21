3f77c93b1541f5dee82b774e793f15fa760cac2b
Joe Nyland <joenyland@me.com>
2017-09-21 08:05:04 +0100

    Move sensorValue declaration into loop()

--- vw-camper.ino
+++ vw-camper.ino
@@ -4,7 +4,6 @@
 RTC_DS3231 rtc;
 
 int sensor = A0;         // Sensor is connected on analogue pin 0
-int sensorValue;         // Where sensor value is stored
 int lights = 3;          // Pin that the lighting system is connected to
 int minDayLevel = 900;   // Min light level for day
 int pollDelay = 1000;    // ms between checking the light level
@@ -16,7 +15,7 @@ void setup() {
 void loop() {
   DateTime now = rtc.now();
   if (now.hour() >= 18 && now.hour() < 22) { // It's currently within hours of operation (18:00 - 22:00)
-    sensorValue = analogRead(sensor);
+    int sensorValue = analogRead(sensor);
     if (sensorValue < minDayLevel) { // Light level is within the desired range
       lightsOn(sensorValue / 8); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
       delay(pollDelay);
