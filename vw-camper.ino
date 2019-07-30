3f62d5a4839d4134233af90a268cf96f7f80522c
Joe Nyland <7666402+JoeNyland@users.noreply.github.com>
2019-07-30 18:27:27 +0100

    New version of the program
    * Prints current time and sensor value out on the serial connection
    * Doesn't have a delay between polls
    * Conditionally sets the RTC if it's lost power
    * Fails to start if RTC has not been found
    * Simpler code and variable names

--- vw-camper.ino
+++ vw-camper.ino
@@ -1,29 +1,45 @@
 #include <Wire.h>
 #include <RTClib.h>
 
-int sensor = A0;         // Sensor is connected on analogue pin 0
-int lights = 3;          // Pin that the lighting system is connected to
-int minDayLevel = 900;   // Min light level for day
-int pollDelay = 1000;    // ms between checking the light level
-RTC_DS3231 rtc;          // Load up RTClib
+int sensorPin = A0;
+int lightsPin = 3;
+int ambientLightThreshold = 200;
+RTC_DS3231 rtc;
 
 void setup() {
-  rtc.begin();
+  Serial.begin(9600);
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+
+  if (rtc.lostPower()) {
+    Serial.println("RTC lost power; setting the time");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
 }
 
 void loop() {
-  DateTime now = rtc.now();
-  if (now.hour() >= 18 && now.hour() < 22) { // It's currently within hours of operation (18:00 - 22:00)
-    int sensorValue = analogRead(sensor);
-    if (sensorValue < minDayLevel) { // Light level is within the desired range
-      digitalWrite(LED_BUILTIN, HIGH);
-      analogWrite(lights, sensorValue / 8); // analogRead values go from 0 to 1023, analogWrite values from 0 to 255
-      delay(pollDelay);
-      return;
-    }
-  }
-  digitalWrite(LED_BUILTIN, LOW);
-  analogWrite(lights, 0);
-  delay(pollDelay);
-}
+  int sensorVal = analogRead(sensorPin);
 
+  DateTime now = rtc.now();
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print("T");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.print(" Sensor:");
+  Serial.print(sensorVal);
+  Serial.println();
+
+  bool output = now.hour() >= 18 && now.hour() < 22 && sensorVal < ambientLightThreshold;
+  digitalWrite(LED_BUILTIN, output);
+  digitalWrite(lightsPin, output);
+}
